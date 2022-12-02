78e72107aae61786efae122882b735044f48a85c
karl-gilmartin <karl_gilmartin@hotmail.com>
2022-12-02 00:39:39 +0000

    + Scanner is now reading barcodes/ qr codes. + It has flash light working + It has pop up dialog before it uses camera
    What Next?
    After Scanning, show details of that product ID number

---
+++ app/src/main/java/com/example/myapplication9/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.myapplication9;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/example/myapplication9/MainActivity.java
+++ app/src/main/java/com/example/myapplication9/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.myapplication9;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button newItemBtn = (Button)findViewById(R.id.NewProduct);
+        Button scanItem = (Button) findViewById(R.id.scanItem);
+
+        scanItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanCode();
+            }
+        });
         newItemBtn.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
@@ -24,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    public void scanCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+    builder.setTitle("Result");
+    builder.setMessage(result.getContents());
+    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            dialogInterface.dismiss();
+        }
+    }).show();
+});
 }
